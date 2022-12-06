9ca2705d0edb2523bfc14b7703cadc011ca01a47
Ben Ridgers <psybr3@nottingham.ac.uk>
2022-12-06 12:07:58 +0000

    added day 6

--- 2022/day4/day4_p2.py
+++ 2022/day4/day4_p2.py
@@ -7,8 +7,7 @@ def main():
     res = 0
     for line in lines:
         pairs = re.split(',|-|\n',line)[:4]
-        count += (a0>=b0 and a0<=b1) or (a1<=b1 and a1>=b0)
-        count += (b0>=a0 and b0<=a1) or (b1<=a1 and b1>=a0)
+        res += overlap(int(pairs[0]),int(pairs[1]),int(pairs[2]),int(pairs[3]))
     print(res)
 
 def overlap(a0,a1,b0,b1):
